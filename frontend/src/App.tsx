0120ff5a4e336bd279afee876f2b15eb55234c8f
Alek M <alek@syserr.com>
2025-09-17 17:49:39 -0400

    Improve R1Tools integration design
    - Remove ugly R1Tools button from navbar
    - Add clean footer with R1Tools link
    - Fix R1Tools section on Home page to use white background card
    - Improve layout consistency and visual hierarchy
    - Remove unused navbar CSS styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
 import { Home } from './pages/Home'
 import { AssetViewer } from './pages/AssetViewer'
 import { CsvConverter } from './pages/CsvConverter'
@@ -20,6 +21,7 @@ function App() {
               <Route path="/about" element={<About />} />
             </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   )
