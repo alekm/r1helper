993fe02406f54f23bf69874030b00c3be5661add
Alek M <alek@syserr.com>
2025-11-04 12:39:28 -0500

    Remove speedtest route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 import { Home } from './pages/Home'
@@ -6,34 +6,23 @@ import { AssetViewer } from './pages/AssetViewer'
 import { CsvConverter } from './pages/CsvConverter'
 import { ApiUploader } from './pages/ApiUploader'
 import { About } from './pages/About'
-import { Speedtest } from './pages/Speedtest'
-
-function AppContent() {
-  const location = useLocation()
-  const isSpeedtestPage = location.pathname === '/speedtest'
-
-  return (
-    <div className="app-container">
-      {!isSpeedtestPage && <Navbar />}
-      <main className={isSpeedtestPage ? '' : 'main-container'}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/assets" element={<AssetViewer />} />
-          <Route path="/csv" element={<CsvConverter />} />
-          <Route path="/upload" element={<ApiUploader />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/speedtest" element={<Speedtest />} />
-        </Routes>
-      </main>
-      {!isSpeedtestPage && <Footer />}
-    </div>
-  )
-}
 
 function App() {
   return (
     <Router>
-      <AppContent />
+      <div className="app-container">
+        <Navbar />
+        <main className="main-container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/assets" element={<AssetViewer />} />
+              <Route path="/csv" element={<CsvConverter />} />
+              <Route path="/upload" element={<ApiUploader />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   )
 }
